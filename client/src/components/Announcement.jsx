a404c4c22526a5c42155f79c676a190859c8debb
Jahid Hiron <namehiron.96@gmail.com>
2022-01-14 22:00:58 +0600

    making slider

---
+++ client/src/components/Announcement.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-style: 14px;
+  font-weight: 500;
+`;
+
+const Announcement = () => {
+  return <Container>Super Deal ! Free shoping on orders over $50</Container>;
+};
+
+export default Announcement;

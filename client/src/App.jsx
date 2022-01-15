a2a1e097df4090a6ab195779ec07895257f23f07
Jahid Hiron <namehiron.96@gmail.com>
2022-01-15 16:22:39 +0600

    cart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@
 // import Product from "./pages/Product";
 // import Register from "./pages/Register";
 // import Login from "./pages/Login";
-import Cart from "./components/Cart";
+import Cart from "./pages/Cart";
 
 function App() {
   return (

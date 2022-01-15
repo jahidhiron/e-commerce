51bb3139c642cc74e32bec68ad9f64e537e03241
Jahid Hiron <namehiron.96@gmail.com>
2022-01-15 16:02:11 +0600

    register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 // import Home from "./pages/Home";
 // import ProductList from "./pages/ProductList";
-import Product from "./pages/Product";
+// import Product from "./pages/Product";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
-      <Product />
+      <Register />
     </div>
   );
 }
